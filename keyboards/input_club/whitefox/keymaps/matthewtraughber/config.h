9b861d8cb1a2615cf0933ce86d5b9011163c9618
Matthew Traughber <me@matthewtraughber.com>
2022-11-05 13:23:28 -0400

    Initial whitefox config

---
+++ keyboards/input_club/whitefox/keymaps/matthewtraughber/config.h
@@ -0,0 +1,11 @@
+// https://docs.qmk.fm/#/getting_started_introduction?id=the-configh-file
+#pragma once
+
+/*
+ * https://docs.qmk.fm/#/tap_hold?id=ignore-mod-tap-interrupt
+ *
+ * Enabling IGNORE_MOD_TAP_INTERRUPT is recommended when using Mod-Tap on alphanumeric keys
+ * to avoid hold function taking precendence when the next key is pressed quickly.
+ */
+#define IGNORE_MOD_TAP_INTERRUPT
+
---
+++ keyboards/input_club/whitefox/keymaps/matthewtraughber/rules.mk
@@ -0,0 +1,8 @@
+# https://docs.qmk.fm/#/squeezing_avr
+LTO_ENABLE = yes
+
+# https://docs.qmk.fm/#/feature_space_cadet?id=caveats
+COMMAND_ENABLE = no
+
+# https://docs.qmk.fm/#/feature_tap_dance?id=tap-dance-a-single-key-can-do-3-5-or-100-different-things
+TAP_DANCE_ENABLE = yes
