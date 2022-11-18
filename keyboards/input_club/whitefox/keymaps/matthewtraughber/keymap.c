bbdaf0cc3b81225fb61f2f565a76876b89967632
Matthew Traughber <me@matthewtraughber.com>
2022-11-18 18:00:44 -0500

    Remove broken LEDTOG_CAPS tapdance

--- keyboards/input_club/whitefox/keymaps/matthewtraughber/keymap.c
+++ keyboards/input_club/whitefox/keymaps/matthewtraughber/keymap.c
@@ -101,8 +101,7 @@ enum {
     LEFT_HOME,
     RGHT_END,
     LSFT_SPACECMDR,
-    RSFT_SPACECMDR,
-    LEDTOG_CAPS
+    RSFT_SPACECMDR
 };
 
 // tap dance definitions
@@ -113,7 +112,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [RGHT_END] = ACTION_TAP_DANCE_DOUBLE(KC_RGHT, KC_END),      // TODO:
     [LSFT_SPACECMDR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lshift_finished, lshift_reset),
     [RSFT_SPACECMDR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rshift_finished, rshift_reset),
-    [LEDTOG_CAPS] = ACTION_TAP_DANCE_DOUBLE(BL_TOGG, KC_CAPS),
 };
 
 // tapping terms for dual-role keys
@@ -161,10 +159,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * └────┴────┴────┴────────────────────────┴────┴────┘ └───┴───┴───┘
     */
   [0] = LAYOUT_truefox( \
-    QK_GESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,            KC_EQL,  KC_DEL,    KC_BSPC,    TD(LEDTOG_CAPS), \
+    QK_GESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,            KC_EQL,  KC_DEL,    KC_BSPC,    BL_TOGG, \
     KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,            KC_RBRC, KC_BSLS,   BL_INC,  \
     LT(1, KC_ESC),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,  BL_DEC,  \
-    TD(LSFT_SPACECMDR), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(RSFT_SPACECMDR), KC_UP,   _______, \
+    TD(LSFT_SPACECMDR), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(RSFT_SPACECMDR), KC_UP,   KC_CAPS, \
     KC_LCTL,            KC_LGUI, KC_LALT,                   KC_SPC,                    MO(1),   KC_RALT, KC_LEFT, KC_DOWN,            KC_RGHT  \
   ),
 
