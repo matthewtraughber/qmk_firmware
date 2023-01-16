e0f2b6eca4683d9722c269282666cca2219f5dfc
Matthew Traughber <me@matthewtraughber.com>
2023-01-16 16:56:11 -0500

    Add Alt/GUI swap for Linux/MacOS

--- keyboards/input_club/whitefox/keymaps/matthewtraughber/rules.mk
+++ keyboards/input_club/whitefox/keymaps/matthewtraughber/rules.mk
@@ -3,3 +3,7 @@ LTO_ENABLE = yes
 
 # https://docs.qmk.fm/#/feature_tap_dance
 TAP_DANCE_ENABLE = yes
+
+# https://docs.qmk.fm/#/feature_bootmagic
+# https://docs.qmk.fm/#/keycodes_magic
+MAGIC_ENABLE = yes
